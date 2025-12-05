ffc1a9273e39c4554b4b3c0cf7b4f37860c6de51
imadsyedvidyamandirorg-jpg <imadsyedvidyamandirorg@gmail.com>
2025-12-05 15:37:51 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
-import React, { useState } from 'react';
-import { SystemState } from './types';
+import { useState } from "react";
 import BootScreen from "./BootScreen";
 import LoginScreen from "./LoginScreen";
 import Desktop from "./Desktop";
 
-const App: React.FC = () => {
-  const [state, setState] = useState<SystemState>(SystemState.BOOT);
+function App() {
+  const [step, setStep] = useState("boot");
 
   return (
     <>
-      {state === SystemState.BOOT && (
-        <BootScreen onComplete={() => setState(SystemState.LOGIN)} />
-      )}
-      
-      {state === SystemState.LOGIN && (
-        <LoginScreen onLogin={() => setState(SystemState.DESKTOP)} />
-      )}
-      
-      {state === SystemState.DESKTOP && (
-        <Desktop />
-      )}
+      {step === "boot" && <BootScreen onFinish={() => setStep("login")} />}
+      {step === "login" && <LoginScreen onLogin={() => setStep("desktop")} />}
+      {step === "desktop" && <Desktop />}
     </>
   );
-};
+}
 
 export default App;
